49c1ba339c7e030fc947bab8773f01b43cc6898d
ricemark20 <46001899+ricemark20@users.noreply.github.com>
2021-02-21 16:01:08 -0600

    Update mkv2mkv.hb-mult-subs.sh
    Corrected variable $1 to $i

--- mkv2mkv.hb-mult-subs.sh
+++ mkv2mkv.hb-mult-subs.sh
@@ -6,5 +6,5 @@ mkdir encoded
 
 for i in *.mkv
 do
-  HandBrakeCLI --native-language eng --subtitle scan,1,2,3,4,5,6,7,8,9,10 --subtitle-default=1 --subtitle-forced=1 --audio-lang-list eng -i $1 -o "encoded"/"`basename -s .mkv $1`.mkv"
+  HandBrakeCLI --native-language eng --subtitle scan,1,2,3,4,5,6,7,8,9,10 --subtitle-default=1 --subtitle-forced=1 --audio-lang-list eng -i $i -o "encoded"/"`basename -s .mkv $i`.mkv"
 done
