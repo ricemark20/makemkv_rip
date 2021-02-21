41d575f622d25a7ee16c922a3c9c595f2ffdf1b4
ricemark20 <46001899+ricemark20@users.noreply.github.com>
2021-02-21 15:28:43 -0600

    Create mkv2mkv.hb-mult-subs.sh

---
+++ mkv2mkv.hb-mult-subs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Convert mkv files in bulk with all available subtitles.
+
+mkdir encoded
+
+for i in *.mkv
+do
+  HandBrakeCLI --native-language eng --subtitle scan,1,2,3,4,5,6,7,8,9,10 --subtitle-default=1 --subtitle-forced=1 --audio-lang-list eng -i $1 -o "encoded"/"`basename -s .mkv $1`.mkv"
+done
