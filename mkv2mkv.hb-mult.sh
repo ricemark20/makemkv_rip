d813a2538c4d60240980f543bc268cc6b95ad663
ricemark20 <46001899+ricemark20@users.noreply.github.com>
2021-02-21 15:31:47 -0600

    Update mkv2mkv.hb-mult.sh

--- mkv2mkv.hb-mult.sh
+++ mkv2mkv.hb-mult.sh
@@ -1,9 +1,10 @@
+#!/bin/bash
+
 # Bash file to bulk convert mkv files using Handbrake
 
-#!/bin/bash
 mkdir encoded
 
 for i in *.mkv
 do
-HandBrakeCLI --first-subtitle --subtitle-default=none -i $i -o "encoded"/"`basename -s .mkv $i`.mkv"
+  HandBrakeCLI --first-subtitle --subtitle-default=none -i $i -o "encoded"/"`basename -s .mkv $i`.mkv"
 done
